8491431ab41e8b9a12ab61afce680726ab7d3a3f
VVF2245 <luihercua@alum.us.es>
2025-12-03 11:03:34 +0100

    Terminado CREATE TABLE (mejor revisad por si cambi)

--- 1.scripts_tablas_restricciones.sql
+++ 1.scripts_tablas_restricciones.sql
@@ -67,25 +67,64 @@ CREATE TABLE Patinetes_Electricos (
 
 CREATE TABLE Estaciones (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    nombre UNIQUE NOT NULL
 );
 
 CREATE TABLE Enganches (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    estacionId INT NOT NULL,
+    numero VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL
+    FOREIGN KEY (estacionId) REFERENCES Estaciones(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT uq_estacion_numero UNIQUE (estacionId, numero)
 );
 
 CREATE TABLE Alquileres (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    clienteId INT NOT NULL,
+    vehiculoId INT NOT NULL,
+    engancheInicioId INT NOT NULL,
+    engancheFinId INT,
+    fechaHoraInicio DATETIME NOT NULL,
+    fechaHoraFin DATETIME,
+    distanciaRecorrida DECIMAL(5,2),
+    costo DECIMAL(5, 2) NOT NULL CHECK (cantidad >= 0),
+    lugarInicio VARCHAR(200) NOT NULL,
+    lugarFin VARCHAR(200),
+    FOREIGN KEY (clienteId) REFERENCES Clientes(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (vehiculoId) REFERENCES Vehiculos(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE Valoraciones (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    alquilerId INT NOT NULL,
+    vehiculoId INT NOT NULL,
+    puntuacion INT NOT NULL,
+    comentario VARCHAR(500),
+    FOREIGN KEY (alquilerId) REFERENCES Alquileres(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (vehiculoId) REFERENCES Vehiculos(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE Reparaciones (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    tecnicoId INT NOT NULL,
+    vehiculoId INT NOT NULL,
+    fecha DATE NOT NULL,
+    detalles VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (tecnicoId) REFERENCES Tecnicos_Mantenimiento(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (vehiculoId) REFERENCES Vehiculos(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
 );
