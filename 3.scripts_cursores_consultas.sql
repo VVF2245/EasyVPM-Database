ee65117990c5a63c885ba9c9e1a9aba6923653f4
NTY8373 <julflocan@alum.us.es>
2025-12-05 16:45:01 +0100

    consultas

--- 3.scripts_cursores_consultas.sql
+++ 3.scripts_cursores_consultas.sql
@@ -4,4 +4,23 @@
 
 
 
-CREATE OR REPLACE VIEW AS top5vehiculos 
+CREATE OR REPLACE VIEW AS listaReparacionesTecnico
+SELECT Usuarios.nombre, COUNT (Reparaciones.id) listaReparaciones
+FROM Usuarios
+JOIN TecnicosMantenimiento ON Usuarios.id=TecnicosMantenimiento.usuarioId
+JOIN Reparaciones ON tecnicoMantenimiento.id=Reparaciones.tecnicoId
+GROUP BY Usuarios.id;
+
+CREATE OR REPLACE VIEW AS listaReparacionesVehiculo
+SELECT Vehiculos.id, COUNT (Reparaciones.id) listaReparaciones
+FROM Vehiculos
+JOIN Reparaciones ON tVehiculo.id=Reparaciones.vehiculoId
+GROUP BY Vehiculos.id;
+
+CREATE OR REPLACE VIEW AS top5vehiculosMasUsados
+SELECT Vehiculos.id FROM Vehiculos
+JOIN Alquileres ON Vehiculos.id=Alquileres.vehiculoId
+GROUP BY Vehiculos.id
+ORDER BY COUNT(Alquileres.id) DESC
+LIMIT 5;
+
